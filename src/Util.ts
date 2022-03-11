0d9614b3065159003362979c240fb492b4824c56
Emma Litwa-Vulcu <80431903+reiniiriarios@users.noreply.github.com>
2022-03-10 17:53:56 -0800

    copyright year

--- src/Util.ts
+++ src/Util.ts
@@ -1,5 +1,5 @@
 // chromaticity-color-utilities
-// Copyright (C) 2021 Emma Litwa-Vulcu
+// Copyright (C) 2022 Emma Litwa-Vulcu
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
